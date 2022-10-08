0202b4ff639b0ae2cdb8e9f11d9cefd2a5a0e1f5
Jrc <mi.mail@gmail.com>
2022-10-08 21:46:19 +0200

    commit

--- src/javi_repo_eoi/Clase_Prueba.java
+++ src/javi_repo_eoi/Clase_Prueba.java
@@ -5,7 +5,7 @@ public class Clase_Prueba {
 	public static void main(String[] args) {
 		
 		System.out.println("Esto es una prueba de git, si.");
-		System.out.println("Una prueba de ver que pasa si hacemos commit con git en local.");
+		System.out.println("Prueba");
 		
 		
 		
