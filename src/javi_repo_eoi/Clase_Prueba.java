763ec6df87c1e654e797d4c7495a0854b9ba674f
Javi <115257367+jrc0git@users.noreply.github.com>
2022-10-08 21:47:38 +0200

    Update Clase_Prueba.java

--- src/javi_repo_eoi/Clase_Prueba.java
+++ src/javi_repo_eoi/Clase_Prueba.java
@@ -5,7 +5,8 @@ public class Clase_Prueba {
 	public static void main(String[] args) {
 		
 		System.out.println("Esto es una prueba de git, si.");
-		System.out.println("Prueba");
+		System.out.println("Prueba en remoto.");
+		
 		
 		
 		}
